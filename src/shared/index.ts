31e90b3b8328b226290e35545b76c6d85db1c480
GUSTAVO FERNANDO AGUILAR FLORES <gaguilarf@unsa.edu.pe>
2025-12-26 10:18:04 -0500

    fix(leadform): add landing page Hero component and update shared component exports

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -2,4 +2,3 @@
 export { default as Header } from './components/Header';
 export { default as Footer } from './components/Footer';
 export { default as BackToTop } from './components/BackToTop';
-export { default as LeadForm } from './components/LeadForm';
